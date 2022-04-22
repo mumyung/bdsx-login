015128469ed062d19257971dd49affc6ca85497d
mumyung <102166413+mumyung@users.noreply.github.com>
2022-04-22 18:46:53 +0900

    Update login.ts

--- login.ts
+++ login.ts
@@ -15,7 +15,6 @@ import * as fs from "fs"
 import { bedrockServer } from "bdsx/launcher"
 import { command } from "bdsx/command"
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
-import { ServerSettingsRequestPacket } from "bdsx/bds/packets"
 import { CommandPermissionLevel } from "bdsx/bds/command";
 import { CANCEL } from "bdsx/common"
 
@@ -23,18 +22,24 @@ events.packetAfter(MinecraftPacketIds.Login).on((ptr, networkIdentifier, packetI
     loginmenu(networkIdentifier);
 })
 command.register("login", "로그인을 합니다.", CommandPermissionLevel.Normal).overload(async (p, o, op) => {
-    let ni = o.getEntity()?.getNetworkIdentifier();
-    loginmenu(ni);
+    const player = o.getEntity();
+    if (player?.isPlayer()) {
+        let ni = player.getNetworkIdentifier()
+        loginmenu(ni);
+    }
 }, {});
 
 command.register("로그인", "로그인을 합니다.", CommandPermissionLevel.Normal).overload(async (p, o, op) => {
-    let ni = o.getEntity()?.getNetworkIdentifier();
-    loginmenu(ni);
+    const player = o.getEntity();
+    if (player?.isPlayer()) {
+        let ni = player.getNetworkIdentifier()
+        loginmenu(ni);
+    }
 }, {});
 
 events.packetBefore(MinecraftPacketIds.Text).on((ptr, ni, id) => {
     if (ni.getActor()?.hasTag(`login`)) {
-        
+
     } else {
         bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c로그인 후 채팅을 치세요!"}]}`)
         return CANCEL
@@ -57,7 +62,7 @@ async function logins(ni: NetworkIdentifier) {
         content: [
             {
                 type: "input",
-                text: "밑에 당신의 닉네임을 써주세요."
+                text: "밑에 당신의 아이디를 써주세요."
             },
             {
                 type: "input",
@@ -71,25 +76,32 @@ async function logins(ni: NetworkIdentifier) {
     if (ni.getActor()?.hasTag(`login`)) {
         bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c당신은 이미 로그인을 했습니다!"}]}`)
     } else {
-        if (name != "") {
-            if (password != "") {
-                const UserJSON = `../plugins/login/User/${name}.json`
-                if (fs.readFileSync(UserJSON, 'utf8')) {
+        if (ni.getActor()?.hasTag(`signup`)) {
+            if (name != "") {
+                if (password != "") {
+                    const UserJSON = `../plugins/login/User/${name}.json`
+                    !fs.existsSync(UserJSON) ? fs.writeFileSync(UserJSON, JSON.stringify({})) : null;
                     const read = JSON.parse(fs.readFileSync(UserJSON, "utf8"))
-                    if (read.password == password) {
-                        bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§a닉네임 : ${name}\n비밀번호 : ${password} 로 로그인했습니다.}"}]}`)
-                        bedrockServer.executeCommand(`tag "${ni.getActor()?.getName()}" add login`)
+                    console.log(read.playerName)
+                    if (read.playerName == undefined) {
+                        fs.unlinkSync(UserJSON);
+                        bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c${name}에 대한 정보가 없습니다!"}]}`)
                     } else {
-                        bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호가 다릅니다!"}]}`)
+                        if (read.password == password) {
+                            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§a아이디 : ${name}\n비밀번호 : ${password}로 로그인했습니다."}]}`)
+                            bedrockServer.executeCommand(`tag "${ni.getActor()?.getName()}" add login`)
+                        } else {
+                            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호가 일치하지 않습니다!"}]}`)
+                        }
                     }
                 } else {
-                    bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c${name}에 대한 정보가 없습니다!"}]}`)
+                    bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호를 적어주세요!"}]}`)
                 }
             } else {
-                bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호를 적어주세요!"}]}`)
+                bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c아이디를 적어주세요!"}]}`)
             }
         } else {
-            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c닉네임을 적어주세요!"}]}`)
+            bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c회원가입 후 시도해주세요!"}]}`)
         }
     }
 }
@@ -101,7 +113,7 @@ async function signup(ni: NetworkIdentifier) {
         content: [
             {
                 type: "input",
-                text: "밑에 닉네임을 써주세요."
+                text: "밑에 아이디를 써주세요."
             },
             {
                 type: "input",
@@ -122,28 +134,94 @@ async function signup(ni: NetworkIdentifier) {
     } else {
         if (name != "") {
             if (password != "") {
-                if (password1 == password) {
-                    const UserJSON = `../plugins/login/User/${name}.json`
-
-                    !fs.existsSync(UserJSON) ? fs.writeFileSync(UserJSON, JSON.stringify({})) : null;
-
-                    let Save = {};
-                    Save = { name, password, deviceId: ni.getActor()?.deviceId, playerName: ni.getActor()?.getName() }
-                    fs.writeFileSync(UserJSON, JSON.stringify(Save))
-                    bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§a당신의 닉네임 : ${name}\n당신의 비밀번호 : ${password} 로 저장되었습니다."}]}`)
-                    bedrockServer.executeCommand(`tag "${ni.getActor()?.getName()}" add signup`)
+                const UserJSON = `../plugins/login/User/${name}.json`
+                !fs.existsSync(UserJSON) ? fs.writeFileSync(UserJSON, JSON.stringify({})) : null;
+                const read = JSON.parse(fs.readFileSync(UserJSON, "utf8"))
+                if (read.playerName) {
+                    bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c${name}은 이미 저장되어있습니다!"}]}`)
                 } else {
-                    bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호가 다릅니다!"}]}`)
+                    if (read.playerName === undefined) {
+                        fs.unlinkSync(UserJSON);
+                        if (password1 == password) {
+                            const UserJSON = `../plugins/login/User/${name}.json`
+
+                            !fs.existsSync(UserJSON) ? fs.writeFileSync(UserJSON, JSON.stringify({})) : null;
+
+                            let Save = {};
+                            Save = { name, password, deviceId: ni.getActor()?.deviceId, playerName: ni.getActor()?.getName() }
+                            fs.writeFileSync(UserJSON, JSON.stringify(Save))
+                            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§a당신의 아이디 : ${name}\n당신의 비밀번호 : ${password}로 저장되었습니다."}]}`)
+                            bedrockServer.executeCommand(`tag "${ni.getActor()?.getName()}" add signup`)
+                        } else {
+                            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호가 일치하지 않습니다!"}]}`)
+                        }
+                    }
                 }
             } else {
                 bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c비밀번호를 적어주세요!"}]}`)
             }
         } else {
-            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c닉네임을 적어주세요!"}]}`)
+            bedrockServer.executeCommand(`tellraw "${ni.getActor()?.getName()}" {"rawtext":[{"text":"§c아이디를 적어주세요!"}]}`)
         }
     }
 }
+async function Deleteaccount(ni: NetworkIdentifier) {
+    const res = await Form.sendTo(ni, {
+        type: "custom_form",
+        title: "§l§b계정 삭제 창",
+        content: [
+            {
+                type: "input",
+                text: "밑에 당신의 아이디를 써주세요",
+            },
+            {
+                type: "input",
+                text: "비번을 입력해 주세요!"
+            }
+        ]
+    })
+    if (res === null) return;
 
+    const name = res[0];
+    const psw = res[1];
+
+    if (ni.getActor()?.hasTag(`signup`)) {
+        if (ni.getActor()?.hasTag(`login`)) {
+            if (name != "") {
+                if (psw != "") {
+                    const UserJSON = `../plugins/login/User/${name}.json`
+                    !fs.existsSync(UserJSON) ? fs.writeFileSync(UserJSON, JSON.stringify({})) : null;
+                    const read = JSON.parse(fs.readFileSync(UserJSON, "utf8"))
+                    if (read.playerName === undefined) {
+                        fs.unlinkSync(UserJSON);
+                        bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c${name}에 대한 정보가 없습니다!"}]}`)
+                    } else {
+                        if (psw == read.password) {
+                            if (ni.getActor()?.deviceId == read.deviceId) {
+                                fs.unlinkSync(UserJSON);
+                                bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§a계정이 삭제 되었습니다!"}]}`)
+                                bedrockServer.executeCommand(`tag "${ni.getActor()!.getName()}" remove signup`)
+                                bedrockServer.executeCommand(`tag "${ni.getActor()!.getName()}" remove login`)
+                            } else {
+                                bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c당신은 해당 계정주인이 아닙니다!"}]}`)
+                            }
+                        } else {
+                            bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c비밀번호가 일치하지 않습니다!"}]}`)
+                        }
+                    }
+                } else {
+                    bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c비밀번호를 적어주세요!"}]}`)
+                }
+            } else {
+                bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c아이디를 적어주세요!"}]}`)
+            }
+        } else {
+            bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c로그인 후 시도해주세요!"}]}`)
+        }
+    } else {
+        bedrockServer.executeCommand(`tellraw "${ni.getActor()!.getName()}" {"rawtext":[{"text":"§c회원가입 후 시도해주세요!"}]}`)
+    }
+}
 async function loginmenu(ni: NetworkIdentifier) {
     const res = await Form.sendTo(ni, {
         type: "form",
@@ -155,14 +233,21 @@ async function loginmenu(ni: NetworkIdentifier) {
             },
             {
                 text: "§l§c회원가입"
+            },
+            {
+                text: "§l§d계정삭제"
             }
         ]
     })
     if (res === null) return;
+
     const login = res;
-    if (login) {
-        signup(ni);
-    } else {
+    console.log(login)
+    if (login == 0) {
         logins(ni);
+    } else if (login == 1) {
+        signup(ni);
+    } else if (login == 2) {
+        Deleteaccount(ni)
     }
 }
